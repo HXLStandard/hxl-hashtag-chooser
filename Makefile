b32554751acd19c6c390bdea555b2504c04b6e84
David Megginson <david.megginson@gmail.com>
2020-05-22 11:57:22 -0400

    Add validation script. Tweak style and opening question.

--- Makefile
+++ Makefile
@@ -3,6 +3,9 @@ VENV=venv/bin/activate
 
 all: docs/index.html
 
+validate: $(VENV)
+	. $(VENV) && python validate-base.py
+
 docs/style.css: style.css
 	cp style.css docs/style.css
 
@@ -12,7 +15,7 @@ docs/script.js: script.js
 docs/icon.png: icon.png
 	cp icon.png docs/icon.png
 
-docs/index.html: $(VENV) hxl-knowledge-base.json gen-chooser.py docs/style.css docs/script.js docs/icon.png
+docs/index.html: $(VENV) validate hxl-knowledge-base.json gen-chooser.py docs/style.css docs/script.js docs/icon.png
 	. $(VENV) && python gen-chooser.py > docs/index.html || rm docs/index.html
 
 # (re)build the virtual environment if it's missing, or whenever setup.py changes
---
+++ validate-base.py
@@ -0,0 +1,80 @@
+""" Validate the HXL knowledge base
+
+Requires Python3.
+
+Usage:
+
+    python validate-base.py
+
+Started 2020-05-22 by David Megginson
+
+"""
+
+import json, logging, sys
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("Validate")
+
+# Requires Python3 or higher
+if sys.version_info < (3, ):
+    raise Exception("Requires Python3 or higher")
+
+def validate (data):
+    """ Validate a JSON knowledge base
+
+    @param data: the data structure to validate
+    @returns: the number of errors logged (0 means no errors)
+    """
+
+    error_count = 0
+
+    def check (key, question, history=[]):
+        """ Recursively check the whole tree """
+
+        nonlocal error_count
+
+        # Check for circular references
+        if key in history:
+            logger.error("Circular reference for question %s: %s", key, str(history + [key]))
+            error_count += 1
+
+        # Check each option
+        for option in question["options"]:
+            if "hashtag" in option:
+                # terminal node
+                return
+            elif "dest" in option:
+                # non-terminal node: check linked question recursively
+                newkey = option["dest"]
+                if newkey in data:
+                    check(newkey, data[newkey], history + [key])
+                else:
+                    logger.error("Destination question %s (from %s) does not exist", newkey, key)
+                    error_count += 1
+            else:
+                # node has neither terminal nor non-terminal attributes
+                logger.error("Option has neither dest nor hashtag in question %s: %s", key, str(option))
+                error_count += 1
+
+    if "top" in data:
+        # Start at the top node
+        check("top", data["top"], [])
+    else:
+        logger.error("No question marked \"top\" in data")
+        error_count += 1
+
+    return error_count
+                    
+        
+#
+# If run from the command line
+#
+if __name__ == "__main__":
+# TODO: specify file on command line
+    with open("hxl-knowledge-base.json", "r") as input:
+        error_count = validate(json.load(input))
+        if error_count > 0:
+            logger.warning("%d error%s", error_count, ("" if error_count == 1 else "s"))
+            sys.exit(1)
+        else:
+            sys.exit(0)
